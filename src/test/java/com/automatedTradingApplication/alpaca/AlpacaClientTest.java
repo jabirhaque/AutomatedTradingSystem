d1792e80499da6eab14a9f460296daffe8a1978f
jabirhaque <jabirhaque@hotmail.co.uk>
2025-07-12 18:14:37 +0100

    testing failing test breaks pipeline

--- src/test/java/com/automatedTradingApplication/alpaca/AlpacaClientTest.java
+++ src/test/java/com/automatedTradingApplication/alpaca/AlpacaClientTest.java
@@ -36,7 +36,7 @@ public class AlpacaClientTest {
     void getQtyFromPriceTest() throws ApiException {
         Mockito.doReturn(211.38).when(alpacaClient).getLatestTradePrice("AAPL");
         double actual = alpacaClient.getQtyFromPrice("AAPL", 500);
-        double expected = 500.00/211.38;
+        double expected = 600.00/211.38;
         logger.debug("Expecting {}, actually {}", expected, actual);
         Assertions.assertEquals(expected, actual);
     }
